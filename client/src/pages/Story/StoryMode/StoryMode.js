76758393660462fa9adc43269880d4c6b6ed9b8a
Chandradithya <chandradithya@gmail.com>
2023-09-16 16:32:39 +0530

    added lottie animations in story mode

--- client/src/pages/Story/StoryMode/StoryMode.js
+++ client/src/pages/Story/StoryMode/StoryMode.js
@@ -1,12 +1,40 @@
 /*** Story Mode UI ***/
 
+// packages
+import Lottie from 'lottie-react'
+
+// css
 import './StoryMode.css'
+
+// components
 import SpeechBubble from './SpeechBubble'
+
+// lottie animations
+import girlTalking from '../../../lotties/girlTalking.json'
 
 const StoryMode = ({ mode }) => {
     return(
         <div className={`story-mode-${mode}`}>
-            <SpeechBubble text={"Welcome to Story Mode!"} />
+            <div className='conversation'>
+                <div className='player-character'>
+                    <SpeechBubble text={""} />
+                    <Lottie
+                        animationData={girlTalking}
+                        loop={true}
+                        autoPlay={true}
+                        style={{ height: 600, width: 600 }}
+                    />
+                </div>
+                <div className='NPC'>
+                    <SpeechBubble text={"Welcome to Story Mode!"} />
+                    <Lottie
+                        animationData={girlTalking}
+                        loop={true}
+                        autoPlay={true}
+                        style={{ height: 600, width: 600 }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }

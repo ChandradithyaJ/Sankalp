aa0053095ac56b0f5d2b3b9473e4c930bdd66f54
Chandradithya <chandradithya@gmail.com>
2023-09-16 17:29:18 +0530

    added response options ui in the story mode

--- client/src/pages/Story/StoryMode/StoryMode.js
+++ client/src/pages/Story/StoryMode/StoryMode.js
@@ -14,14 +14,29 @@ import SpeechBubble from './SpeechBubble'
 import girlTalking from '../../../lotties/girlTalking.json'
 
 const StoryMode = ({ mode }) => {
+    // variable dimensions for the lottie animation
     const [lottieDim, setLottieDim] = useState(600)
+    // state to keep track of the selected response option
+    const [selectedOption, setSelectedOption] = useState('')
+
+    // test response options
+    const options = ['a', 'b', 'c', 'd']
+
+    // select a response option
+    const clickOnOption = (option) => {
+        if(option !== selectedOption){
+            setSelectedOption(option)
+        } else{
+            setSelectedOption('')
+        }
+    }
 
     return(
         <div className={`story-mode-${mode}`}>
             <div className='conversation'>
                 <div className='player-character'>
                     <div className='player-character-ui'>
-                        <SpeechBubble text={""} />
+                        <SpeechBubble text={selectedOption} />
                         <div className='player-lottie'>
                             <Lottie
                                 animationData={girlTalking}
@@ -32,7 +47,14 @@ const StoryMode = ({ mode }) => {
                         </div>
                     </div>
                     <div className='player-character-options'>
-                        
+                        {options.map((option) => (
+                            <div
+                                className={selectedOption === option ? 'selectedOption' : 'option'}
+                                onClick={() => clickOnOption(option)}
+                            >
+                                {option}
+                            </div>
+                        ))}
                     </div>
                 </div>  
                 <div className='NPC'>

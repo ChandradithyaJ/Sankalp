35a0defcbb173982295e9f53d366a5fcc2362824
Chandradithya <chandradithya@gmail.com>
2023-09-16 17:12:04 +0530

    responsive story mode

--- client/src/pages/Story/StoryMode/StoryMode.js
+++ client/src/pages/Story/StoryMode/StoryMode.js
@@ -2,6 +2,7 @@
 
 // packages
 import Lottie from 'lottie-react'
+import { useState } from 'react'
 
 // css
 import './StoryMode.css'
@@ -13,26 +14,37 @@ import SpeechBubble from './SpeechBubble'
 import girlTalking from '../../../lotties/girlTalking.json'
 
 const StoryMode = ({ mode }) => {
+    const [lottieDim, setLottieDim] = useState(600)
+
     return(
         <div className={`story-mode-${mode}`}>
             <div className='conversation'>
                 <div className='player-character'>
-                    <SpeechBubble text={""} />
-                    <Lottie
-                        animationData={girlTalking}
-                        loop={true}
-                        autoPlay={true}
-                        style={{ height: 600, width: 600 }}
-                    />
-                </div>
+                    <div className='player-character-ui'>
+                        <SpeechBubble text={""} />
+                        <div className='player-lottie'>
+                            <Lottie
+                                animationData={girlTalking}
+                                loop={true}
+                                autoPlay={true}
+                                style={{ height: { lottieDim }, width: { lottieDim } }}
+                            />
+                        </div>
+                    </div>
+                    <div className='player-character-options'>
+                        
+                    </div>
+                </div>  
                 <div className='NPC'>
                     <SpeechBubble text={"Welcome to Story Mode!"} />
-                    <Lottie
-                        animationData={girlTalking}
-                        loop={true}
-                        autoPlay={true}
-                        style={{ height: 600, width: 600 }}
-                    />
+                    <div className='npc-lottie'>
+                        <Lottie
+                            animationData={girlTalking}
+                            loop={true}
+                            autoPlay={true}
+                            style={{ height: { lottieDim }, width: { lottieDim } }}
+                        />
+                    </div>
                 </div>
             </div>
         </div>

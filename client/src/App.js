96d148be16c5caf09e89cdc7a31a16ed8d346aa1
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2023-09-18 17:15:36 +0530

    login UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StoryMode from "./pages/Story/StoryMode/StoryMode";
-import Login from './pages/Authentication/login';
-import Signup from './pages/Authentication/signup';
-import ResetPassword from './pages/Authentication/reset-password';
+import Login from "./pages/Authentication/login";
+import Signup from "./pages/Authentication/signup";
+import ResetPassword from "./pages/Authentication/reset-password";
 import Home from "./pages/Home/Home";
 import ChatBox from "./pages/ChatBox/ChatBox";
 import LandingPage from "./pages/LandingPage/LandingPage";
@@ -14,34 +14,46 @@ function App() {
   const [mode, setMode] = useState("dark");
 
   // Using the API created with Node and Express to fetch all users
-  const USERS_API_URL = 'http://localhost:3500/users'
-  const [users, setUsers] = useState([])
+  const USERS_API_URL = "http://localhost:3500/users";
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     const fetchUsers = async () => {
-      try{
-        const response = await fetch(USERS_API_URL)
-        const allUsers = await response.json()
+      try {
+        const response = await fetch(USERS_API_URL);
+        const allUsers = await response.json();
 
-        console.log('Users fetched: ', allUsers)
-        setUsers(allUsers)
+        console.log("Users fetched: ", allUsers);
+        setUsers(allUsers);
       } catch (err) {
-        console.log('Error: err.message')
+        console.log("Error: err.message");
       }
-    }
+    };
 
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
   return (
     <div className={`App-${mode}`}>
-      <Navbar />
+      <Navbar mode={mode} setMode={setMode} />
       <Routes>
         <Route exact path="home" element={<Home />} />
-        <Route exact path="/ChatBox" element={<ChatBox/>} />
-        <Route exact path="BlogPage" element={<BlogPage/>} />
-        <Route exact path="login" element={<Login mode={mode} setMode={setMode}/>} />
-        <Route exact path="signup" element={<Signup mode={mode} setMode={setMode}/>} />
-        <Route exact path="reset-password" element={<ResetPassword mode={mode} setMode={setMode}/>} />
+        <Route exact path="/ChatBox" element={<ChatBox />} />
+        <Route exact path="BlogPage" element={<BlogPage />} />
+        <Route
+          exact
+          path="login"
+          element={<Login mode={mode} setMode={setMode} />}
+        />
+        <Route
+          exact
+          path="signup"
+          element={<Login mode={mode} setMode={setMode} />}
+        />
+        <Route
+          exact
+          path="reset-password"
+          element={<ResetPassword mode={mode} setMode={setMode} />}
+        />
         <Route exact path="therapy-chatbot" element={<div></div>} />
         <Route exact path="story" element={<div></div>} />
         <Route exact path="story/modules" element={<div></div>} />
--- client/src/pages/Authentication/login.js
+++ client/src/pages/Authentication/login.js
@@ -1,34 +1,154 @@
-import React, { useState } from 'react';
-import './Authentication_style.css';
+import React, { useState } from "react";
+// import "./Authentication_style.css";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 
-function Login({mode,setMode}) {
-  const toggleMode = () => {
-    setMode(mode === 'light' ? 'dark' : 'light'); // Toggle between light and dark mode
+// function Login({ mode, setMode }) {
+// const toggleMode = () => {
+//   setMode(mode === "light" ? "dark" : "light"); // Toggle between light and dark mode
+// };
+
+// return (
+//   <div className={`Login Login--${mode}`}>
+//     <div className="Login__container">
+//       <div className="Login__card">
+//         <h1>Login</h1>
+//         <form>
+//           <div className="Login__input">
+//             <h5>E-mail</h5>
+//             <input type="text" />
+//           </div>
+//           <div className="Login__input">
+//             <h5>Password</h5>
+//             <input type="password" />
+//           </div>
+//           <button type="submit" className="Login__signInButton">
+//             Sign In
+//           </button>
+//         </form>
+//         <button className="Login__registerButton">Create your Account</button>
+//       </div>
+//     </div>
+//     <button onClick={toggleMode}>Toggle Mode</button>
+//   </div>
+// );
+// }
+function Login({ mode }) {
+  // const toggleMode = () => {
+  //   setMode(mode === "light" ? "dark" : "light"); // Toggle between light and dark mode
+  // };
+  const navigate = useNavigate();
+  const [showSignup, setShowSignup] = useState(false);
+
+  const toggleView = () => {
+    setShowSignup(!showSignup);
+    const newPath = showSignup ? "/login" : "/signup";
+    navigate(`${newPath}`);
+  };
+
+  const handleCheckboxClick = () => {
+    // You can perform some actions here when the checkbox is clicked
+    console.log("Checkbox clicked!");
   };
 
   return (
-    <div className={`Login Login--${mode}`}>
-      <div className="Login__container">
-        <div className="Login__card">
-          <h1>Login</h1>
-          <form>
-            <div className="Login__input">
-              <h5>E-mail</h5>
-              <input type="text" />
+    <div className={`section login-${mode}`}>
+      <h6>
+        <span>Log In </span>
+        <span>Sign Up</span>
+      </h6>
+      <input
+        className="checkbox"
+        type="checkbox"
+        id="reg-log"
+        name="reg-log"
+        checked={showSignup}
+        onChange={toggleView}
+        onClick={handleCheckboxClick}
+      />
+      <label htmlFor="reg-log"></label>
+      <div className="card-3d-wrap">
+        <div className="card-3d-wrapper">
+          <div className="card-front">
+            <div className="center-wrap">
+              <h4>Log In</h4>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="logemail"
+                  className="form-style"
+                  placeholder="Your Email"
+                  id="logemail"
+                  autoComplete="off"
+                />
+                <i className="input-icon"></i>
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="logpass"
+                  className="form-style"
+                  placeholder="Your Password"
+                  id="logpass"
+                  autoComplete="off"
+                />
+                <i className="input-icon"></i>
+              </div>
+              <a href="#" className="btn">
+                submit
+              </a>
+              <p>
+                <a href="#0" className="link">
+                  Forgot your password?
+                </a>
+              </p>
             </div>
-            <div className="Login__input">
-              <h5>Password</h5>
-              <input type="password" />
+          </div>
+          <div className="card-back">
+            <div className="center-wrap">
+              <h4>Sign Up</h4>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="logname"
+                  className="form-style"
+                  placeholder="Your Full Name"
+                  id="logname"
+                  autoComplete="off"
+                />
+                <i className="input-icon"></i>
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="logemail"
+                  className="form-style"
+                  placeholder="Your Email"
+                  id="logemail"
+                  autoComplete="off"
+                />
+                <i className="input-icon"></i>
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="logpass"
+                  className="form-style"
+                  placeholder="Your Password"
+                  id="logpass"
+                  autoComplete="off"
+                />
+                <i className="input-icon"></i>
+              </div>
+              <a href="#" className="btn">
+                submit
+              </a>
             </div>
-            <button type="submit" className="Login__signInButton">
-              Sign In
-            </button>
-          </form>
-          <button className="Login__registerButton">Create your Account</button>
+          </div>
         </div>
       </div>
-      <button onClick={toggleMode}>Toggle Mode</button>
     </div>
+    // </div>
   );
 }
 

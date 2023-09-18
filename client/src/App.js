cb017d5f8eddd786b65ef501b629d4e2340a4ff2
Chandradithya <chandradithya@gmail.com>
2023-09-18 17:20:11 +0530

    tested all required API methods, working, updated to axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import usersAPI from "./api/usersAPI";
 import Navbar from "./components/Navbar";
 import StoryMode from "./pages/Story/StoryMode/StoryMode";
 import Login from './pages/Authentication/login';
@@ -7,30 +8,29 @@ import Signup from './pages/Authentication/signup';
 import ResetPassword from './pages/Authentication/reset-password';
 import Home from "./pages/Home/Home";
 import ChatBox from "./pages/ChatBox/ChatBox";
-import LandingPage from "./pages/LandingPage/LandingPage";
 import BlogPage from "./pages/Blog/Blog";
 
 function App() {
   const [mode, setMode] = useState("dark");
 
+  /*
   // Using the API created with Node and Express to fetch all users
-  const USERS_API_URL = 'http://localhost:3500/users'
   const [users, setUsers] = useState([])
   useEffect(() => {
     const fetchUsers = async () => {
       try{
-        const response = await fetch(USERS_API_URL)
-        const allUsers = await response.json()
-
-        console.log('Users fetched: ', allUsers)
-        setUsers(allUsers)
+        const response = await usersAPI.get('/users')
+        if(response && response.data){
+          console.log('Users fetched: ', response.data)
+          setUsers(response.data)
+        }
       } catch (err) {
-        console.log('Error: err.message')
+        console.log(`Error: ${err.message}`)
       }
     }
 
     fetchUsers()
-  }, [])
+  }, []) */
 
   return (
     <div className={`App-${mode}`}>

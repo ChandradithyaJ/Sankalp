3d13add21002fafde160cc0181f2332c994d112e
Chandradithya <chandradithya@gmail.com>
2023-09-22 17:33:37 +0530

    create basic outline for the profile page

--- client/src/pages/UserProfile/UserProfile.js
+++ client/src/pages/UserProfile/UserProfile.js
@@ -1,11 +1,23 @@
+import './UserProfile.css'
+
 const UserProfile = ({mode, setMode}) => {
     return(
         <div className={`profile-page-${mode}`}>
-            <div className={`user-details`}>
-
-            </div>
-            <div className={`other-user-details`}>
-
+            <div className='user-details'>
+                <div className='user-background-image'></div>
+                <div className='profile-pic-container'>
+                    <img 
+                        className={`profile-pic-${mode}`}
+                        src={`./images/anonymousProfilePic${mode}.jpg`}
+                        alt='anonymousProfilePic.jpg'
+                    />
+                    <p className='username'>
+                        Username
+                    </p>
+                    <div>
+                        <p>Bekvjbeqtidwmklw dkwnevrweodnske3dw kdwf irfnedsoiefdnobfcow  oifheiwjdplxmst4 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
+                    </div>
+                </div>
             </div>
         </div>
     )

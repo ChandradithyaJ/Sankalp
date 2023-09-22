b1fcbb2045e38eb64f6a34b64e5c7db608508400
Chandradithya <chandradithya@gmail.com>
2023-09-22 18:57:39 +0530

    finished profile page ui

--- client/src/pages/UserProfile/UserProfile.js
+++ client/src/pages/UserProfile/UserProfile.js
@@ -1,6 +1,12 @@
 import './UserProfile.css'
 
 const UserProfile = ({mode, setMode}) => {
+    const badgeImages = ['./images/greenCheck.png', './images/greenCheck.png', './images/greenCheck.png', './images/greenCheck.png', './images/greenCheck.png', './images/greenCheck.png', './images/greenCheck.png']
+
+    const editProfile= () => {
+
+    }
+
     return(
         <div className={`profile-page-${mode}`}>
             <div className='user-details'>
@@ -14,9 +20,27 @@ const UserProfile = ({mode, setMode}) => {
                     <p className='username'>
                         Username
                     </p>
-                    <div>
-                        <p>Bekvjbeqtidwmklw dkwnevrweodnske3dw kdwf irfnedsoiefdnobfcow  oifheiwjdplxmst4 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
+                    <div className='bio'>
+                        <p>Bio</p>
                     </div>
+                    <div 
+                        className={`edit-button-profile-${mode}`}
+                        onClick={editProfile}
+                    >
+                        Edit Profile
+                    </div>
+                </div>
+                <div className='badges-container'>
+                    {badgeImages.map((badgeImage) => (
+                        <div className='one-badge-container'>
+                            <img
+                                className={`badge-image`}
+                                src={badgeImage}
+                                alt={badgeImage}
+                            />
+                            <p>Finish 8 stories!</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>

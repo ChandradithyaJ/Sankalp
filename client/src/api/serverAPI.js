a37e2c4703fa7a8270d45bfdcb5f653d967121d2
Chandradithya <chandradithya@gmail.com>
2023-10-10 00:14:36 +0530

    added a testing API in react for local host testing

--- client/src/api/serverAPI.js
+++ client/src/api/serverAPI.js
@@ -2,4 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: 'https://sankalp-api.onrender.com/'
+    // baseURL: 'https://localhost:3500/'
 })
---
+++ client/src/api/testingAPI.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export default axios.create({
+    // baseURL: 'https://sankalp-api.onrender.com/'
+    baseURL: 'https://localhost:3500/'
+})

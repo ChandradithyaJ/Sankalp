b79504c8624042f5bc355ba399d143002f75b300
Chandradithya <chandradithya@gmail.com>
2023-10-09 22:10:47 +0530

    config vercel

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,10 +2,12 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://sankalp.onrender.com',
+    'http://sankalp.onrender.com'
 ]
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))

155dfed722183f91e62ce87992377c76361ab99c
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2023-11-03 00:05:50 +0530

    Revert "therapy chatbot"
    This reverts commit b4e4be7b4327d24a178ef6d3ea5c0dd7cb644457.

--- client/src/api/testingAPI.js
+++ client/src/api/testingAPI.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:3500/",
-});
-=======
 import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://sankalp-api.onrender.com/'
     baseURL: 'https://localhost:3500/'
 })
->>>>>>> 1b1ba55c5744cb8bcb583c3ea8e1b52f10ebf37c

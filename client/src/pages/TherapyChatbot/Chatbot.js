d2f11967de5defcab7e28cd68519b81b41ce0485
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2023-11-10 18:12:32 +0530

    responsiveness changes

--- client/src/pages/TherapyChatbot/Chatbot.js
+++ client/src/pages/TherapyChatbot/Chatbot.js
@@ -6,7 +6,6 @@ import { AiOutlineFontSize, AiOutlineSend } from "react-icons/ai";
 import "./Chatbot.css";
 
 const Chatbot = ({ mode }) => {
-
   let eliza = new ElizaBot();
   let sentiment = new Sentimood();
 
@@ -14,9 +13,8 @@ const Chatbot = ({ mode }) => {
   const displayRows = 20;
   const [userSentiment, setUserSentiment] = useState(0);
 
-
-  const [eDisplay, setEDisplay] = useState('');
-  const [eInput, setEInput] = useState('');
+  const [eDisplay, setEDisplay] = useState("");
+  const [eInput, setEInput] = useState("");
   const [elizaLines, setElizaLines] = useState([]);
   let savedMessages;
   let savedMessagesArray = [];
@@ -32,16 +30,16 @@ const Chatbot = ({ mode }) => {
   useEffect(() => {
     if (elizaLines.length > 0) {
       savedMessagesArray.concat(JSON.stringify(elizaLines));
-      localStorage.setItem('elizaChatMessages', JSON.stringify(elizaLines));
+      localStorage.setItem("elizaChatMessages", JSON.stringify(elizaLines));
     }
   }, [elizaLines]);
 
   const imgs = {
-     "0" : "./MiaImages/default-01.png",
-     "1": "./MiaImages/happy1-01.png",
-     "2": "./MiaImages/happy2-01.png",
-     "3": "./MiaImages/happy3-01.png",
-    "-1" : "./MiaImages/sad1-01.png",
+    0: "./MiaImages/default-01.png",
+    1: "./MiaImages/happy1-01.png",
+    2: "./MiaImages/happy2-01.png",
+    3: "./MiaImages/happy3-01.png",
+    "-1": "./MiaImages/sad1-01.png",
     "-2": "./MiaImages/sad2-01.png",
     "-3": "./MiaImages/sad3-01.png",
   };
@@ -51,12 +49,12 @@ const Chatbot = ({ mode }) => {
     } else {
       setUserSentiment(val);
     }
-  
+
     // Ensure userSentiment is within the range -3 to 3
     setUserSentiment((prevSentiment) =>
       Math.min(3, Math.max(-3, prevSentiment))
     );
-    
+
     // Update image
     // console.log(val);
     document.getElementById("main-image").src = imgs[userSentiment];
@@ -81,7 +79,7 @@ const Chatbot = ({ mode }) => {
     let rpl;
     let updatedElizaLines;
     setElizaLines(updatedElizaLines);
-    console.log (sentiment.analyze(userinput)["score"]);
+    console.log(sentiment.analyze(userinput)["score"]);
     updateImage(sentiment.analyze(userinput)["score"]);
 
     if (eliza.quit) {
@@ -89,8 +87,7 @@ const Chatbot = ({ mode }) => {
       if (window.confirm("This session is over.\nStart over?")) elizaReset(e);
       f.e_input.focus();
       return;
-    }
-    else if (userinput !== "") {
+    } else if (userinput !== "") {
       console.log(userinput);
       usr = "YOU: " + userinput;
       // console.log(eliza.getFinal("sad"));
@@ -123,32 +120,22 @@ const Chatbot = ({ mode }) => {
     setEInput("");
   };
 
-
   return (
     <div className={`chatbot-container-${mode}`}>
       {/* <button onClick={elizaReset}>Reset</button> */}
       <div className={`chatbot-left-${mode}`}>
-
-        <div className="chatbot-header">
-          A better approach is to place a regular submit button outside the input, then style things to make it look like the button is inside. That will also preserve accessibility (e.g. blind users will be able to use your website), and pressing Enter will submit your form automatically across all browsers. See the below code, or check out this jsFiddle for a working proof-of-concept.
-        </div>
         <div className={`chatbot-image-${mode}`}>
-          <img
-            id="main-image"
-            src={"./MiaImages/default-01.png"}
-            alt="Mia"
-          />
+          <img id="main-image" src={"./MiaImages/default-01.png"} alt="Mia" />
+          <h3>Mia</h3>
         </div>
       </div>
       <div className={`chatbot-body-${mode}`}>
         <div className={`chatbot-chat-${mode}`}>
           <form name="e_form" onSubmit={elizaStep}>
             <textarea
-
               name="e_display"
               id="e_display"
               value={elizaLines?.join("\n")}
-
               onChange={(e) => setEDisplay(e.target.value)}
               readOnly
             ></textarea>
@@ -160,18 +147,23 @@ const Chatbot = ({ mode }) => {
                 id="e_input"
                 value={eInput}
                 placeholder="Enter your message here"
-                onChange={(e) => setEInput(e.target.value)
-                }
-
-
+                onChange={(e) => setEInput(e.target.value)}
               />
-              <AiOutlineSend type="submit" onClick={elizaStep} style={{ color: 'lightblue', fontSize: '4vh', justifyContent: "normal", margin: "7px" }}></AiOutlineSend>
-
+              <AiOutlineSend
+                type="submit"
+                onClick={elizaStep}
+                style={{
+                  color: "lightblue",
+                  fontSize: "4vh",
+                  justifyContent: "normal",
+                  margin: "7px",
+                }}
+              ></AiOutlineSend>
             </div>
           </form>
         </div>
       </div>
-    </div >
+    </div>
   );
-}
+};
 export default Chatbot;
